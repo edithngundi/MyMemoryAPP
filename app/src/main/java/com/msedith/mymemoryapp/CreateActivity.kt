83a4dbab63b4ceb1ecf0f87b4114a906c4be5139
edithngundi <edithngundi@gmail.com>
2024-04-30 21:22:55 -0700

    Custom Photos for the game
    Users can now add their custom photos from Google Photos

--- app/src/main/java/com/msedith/mymemoryapp/CreateActivity.kt
+++ app/src/main/java/com/msedith/mymemoryapp/CreateActivity.kt
@@ -1,24 +1,86 @@
 package com.msedith.mymemoryapp
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.msedith.mymemoryapp.models.BoardSize
 import com.msedith.mymemoryapp.utils.EXTRA_BOARD_SIZE
+import com.msedith.mymemoryapp.utils.isPermissionGranted
+import com.msedith.mymemoryapp.utils.requestPermission
 
 class CreateActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "CreateActivity"
+        private const val PICK_PHOTO_CODE = 655
+        @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+        private const val READ_PHOTOS_PERMISSION = android.Manifest.permission.READ_MEDIA_IMAGES
+        private const val READ_IMAGES_CODE = 755
+    }
 
+    private lateinit var rvImagePicker: RecyclerView
+    private lateinit var etGameName: EditText
+    private lateinit var btnSave: Button
+
+    private lateinit var adapter : ImagePickerAdapter
     private lateinit var boardSize : BoardSize
     private var numImagesRequired = -1
+    private val chosenImageUris = mutableListOf<Uri>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
+        rvImagePicker = findViewById(R.id.rvImagePicker)
+        etGameName = findViewById(R.id.etGameName)
+        btnSave = findViewById(R.id.btnSave)
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         boardSize = intent.getSerializableExtra(EXTRA_BOARD_SIZE) as BoardSize
         numImagesRequired = boardSize.getNumPairs()
         supportActionBar?.title = "Choose pics (0 / $numImagesRequired)"
+
+        adapter = ImagePickerAdapter(this, chosenImageUris, boardSize, object: ImagePickerAdapter.ImageClickListener{
+            @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+            override fun onPlaceholderClicked() {
+                if (isPermissionGranted(this@CreateActivity, READ_PHOTOS_PERMISSION)){
+                    launchIntentForPhotos()
+                } else{
+                    requestPermission(this@CreateActivity, READ_PHOTOS_PERMISSION,READ_IMAGES_CODE)
+                }
+            }
+        })
+        rvImagePicker.adapter = adapter
+        rvImagePicker.setHasFixedSize(true)
+        rvImagePicker.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == READ_IMAGES_CODE){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                launchIntentForPhotos()
+            } else{
+                Toast.makeText(this,
+                    "Inorder to create a custom game, you need to provide access to your photos",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -27,5 +89,41 @@ class CreateActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode != PICK_PHOTO_CODE || resultCode != Activity.RESULT_OK || data == null){
+            Log.w(TAG, "Did not get data back from launched activity, user likely canceled the selection flow")
+            return
+        }
+        val selectedUri = data.data
+        val clipData = data.clipData
+        if (clipData != null){
+            Log.i(TAG, "clipData numImages ${clipData.itemCount}: $clipData")
+            for (i in 0 until clipData.itemCount){
+                val clipItem = clipData.getItemAt(i)
+                if (chosenImageUris.size < numImagesRequired){
+                    chosenImageUris.add(clipItem.uri)
+                }
+            }
+        } else if (selectedUri != null){
+            Log.i(TAG, "data: $selectedUri")
+            chosenImageUris.add(selectedUri)
+        }
+        adapter.notifyDataSetChanged()
+        supportActionBar?.title = "Choose pics ${chosenImageUris.size} / $numImagesRequired"
+        btnSave.isEnabled = shouldEnableSaveButton()
+    }
+
+    private fun shouldEnableSaveButton(): Boolean {
+        return true
+    }
+
+    private fun launchIntentForPhotos() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        startActivityForResult(Intent.createChooser(intent, "Choose pictures"), PICK_PHOTO_CODE)
     }
 }
