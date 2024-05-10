5cd7b5bb2b76bf1247c4b9744f210492b22bb1a1
edithngundi <edithngundi@gmail.com>
2024-05-10 13:04:03 -0700

    Add ability to download a game
    Add ability to download a game

--- app/src/main/java/com/msedith/mymemoryapp/MainActivity.kt
+++ app/src/main/java/com/msedith/mymemoryapp/MainActivity.kt
@@ -10,10 +10,12 @@ import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.EditText
 import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -25,6 +27,7 @@ import com.google.firebase.firestore.firestore
 import com.msedith.mymemoryapp.models.UserImageList
 import com.msedith.mymemoryapp.utils.EXTRA_BOARD_SIZE
 import com.msedith.mymemoryapp.utils.EXTRA_GAME_NAME
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -32,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         private const val CREATE_REQUEST_CODE = 248
     }
 
-    private lateinit var clRoot : ConstraintLayout
+    private lateinit var clRoot : CoordinatorLayout
     private lateinit var rvBoard : RecyclerView
     private lateinit var tvNumMoves : TextView
     private lateinit var tvNumPairs : TextView
@@ -80,6 +83,10 @@ class MainActivity : AppCompatActivity() {
                 showCreationDialog()
                 return true
             }
+            R.id.mi_download -> {
+                showDownloadDialog()
+                return true
+            }
         }
         return super.onOptionsItemSelected(item)
     }
@@ -96,19 +103,33 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
     }
 
+    private fun showDownloadDialog() {
+        val boardDownloadView = LayoutInflater.from(this).inflate(R.layout.dialog_download_board, null)
+        showAlertDialog("Fetch memory game", boardDownloadView, View.OnClickListener {
+            // Grab the text of the game name that the user wants to download
+            val etDownloadGame = boardDownloadView.findViewById<EditText>(R.id.etDownloadGame)
+            val gameToDownload = etDownloadGame.text.toString().trim()
+            downloadGame(gameToDownload)
+        })
+    }
+
     private fun downloadGame(customGameName: String) {
         db.collection("games").document(customGameName).get().addOnSuccessListener { document ->
             val userImageList = document.toObject(UserImageList::class.java)
             if (userImageList?.images == null){
                 Log.e(TAG, "Invalid custom game data from Firestore")
-                Snackbar.make(clRoot, "Sorry, we couldn't find any such game, '$gameName'", Snackbar.LENGTH_LONG).show()
+                Snackbar.make(clRoot, "Sorry, we couldn't find any such game, '$customGameName'", Snackbar.LENGTH_LONG).show()
                 return@addOnSuccessListener
             }
             val numCards = userImageList.images.size * 2
             boardSize = BoardSize.getByValue(numCards)
             customGameImages = userImageList.images
-            setupBoard()
+            for (imageUrl in userImageList.images) {
+                Picasso.get().load(imageUrl).fetch()
+            }
+            Snackbar.make(clRoot, "You're now playing '$customGameName'!", Snackbar.LENGTH_LONG).show()
             gameName = customGameName
+            setupBoard()
         }.addOnFailureListener { exception ->
             Log.e(TAG, "Exception when retrieving game", exception)
         }
